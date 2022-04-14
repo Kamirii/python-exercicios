0036a0f52c100e79177f012e0693b20c7833f639
Kamirii <you@example.com>
2022-04-14 14:00:09 -0300

    teste

--- math_challenge.py
+++ math_challenge.py
@@ -23,7 +23,7 @@ if respostaInput == resposta:
     cont = cont + 1
 else:
   erro = erro + 1
-print("subtraia os seguintes valores:")
+print("subbbtraia os seguintes valores:")
 print(a)
 print(b)
 resposta = sub
