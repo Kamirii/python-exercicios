2063567ffaf67a139a0fa7e6d09aee3878e24fbb
Kamirii <you@example.com>
2022-04-15 13:41:43 -0300

    feito

--- math_challenge.py
+++ math_challenge.py
@@ -2,53 +2,75 @@
 # O jogo consiste em um desafio matemático no qual deve-se responder corretamente as quatro operações básicas.
 #Versão dinamica 
 import random
+from timeit import repeat
 a = random.randint(1,5)
 b = random.randint(1,5)
 respostaInput = input()
-resposta = int
+resposta = float
 soma = a+b
 div= a/b
 multi = a*b
 sub = a-b
-cont = 0
+acerto = 0
 erro = 0
 
+
 print("Você deve responder corretamente as 8 perguntas de operações básicas: ")
-print("some os seguintes valores:")
-print(a)
-print(b)
-resposta = soma
-respostaInput = int(input("Insira sua resposta:"))
-if respostaInput == resposta:
-    cont = cont + 1
-else:
-  erro = erro + 1
-print("subtraia os seguintes valores:")
-print(a)
-print(b)
-resposta = sub
-respostaInput = int(input("Insira sua resposta:"))
-if respostaInput == resposta:
-    cont = cont + 1
-else:
-    erro= erro +1    
-print("multiplique os seguintes valores:")
-print(a)
-print(b)
-resposta = multi
-respostaInput = int(input("Insira sua resposta:"))
-if respostaInput == resposta:
-    cont = cont + 1
-else:
-    erro= erro +1
-print("Divida os seguintes valores:")
-print(a)
-print(b)
-resposta = div
-respostaInput = int(input("Insira sua resposta:"))
-if respostaInput == resposta:
-    cont = cont + 1
-else:
-    erro= erro +1
-print(cont , "acertos")
-print(erro , "erros")
+
+while(respostaInput != resposta):
+ while(True):
+  print("some os seguintes valores:")
+  print(a)
+  print(b)
+  resposta = soma
+  respostaInput = float(input("Insira sua resposta:"))
+  if respostaInput == resposta:
+    acerto = acerto =+ 1
+    break
+  else:
+   erro = erro =+ 1
+   print("Errado.Refaça") 
+  repeat
+ while(True):
+  print("subtraia os seguintes valores:")
+  print(a)
+  print(b)
+  resposta = sub
+  respostaInput = float(input("Insira sua resposta:"))
+  if respostaInput == resposta:
+    acerto = acerto =+ 1
+    break
+  else:
+    erro= erro =+ 1    
+    print("Errado.Refaça") 
+    repeat
+ while(True):
+   print("Multiplique os seguintes valores:")
+   print(a)
+   print(b)
+   resposta = multi
+   respostaInput = float(input("Insira sua resposta:"))
+   if respostaInput == resposta:
+     acerto = acerto =+ 1
+     break
+   else:
+    erro = erro =+ 1
+    print("Errado.Refaça") 
+   repeat
+ while(True):
+   print("Divida os seguintes valores:")
+   print(a)
+   print(b)
+   resposta = div
+   respostaInput = float(input("Insira sua resposta:"))
+   if respostaInput == resposta:
+     acerto = acerto =+ 1
+     break
+   else:
+     erro = erro =+ 1
+     print("Errado.Refaça") 
+     repeat
+if(acerto > erro):
+    print("Parabéns. VocÊ completou o teste com sucesso")
+    
+
